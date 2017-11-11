8571155cdd5b6e9a4c1c8f0724073968f86b707e
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-11-11 18:59:49 +0200

    Update gpaCalc.py

--- gpaCalc.py
+++ gpaCalc.py
@@ -2,7 +2,7 @@ print("Welcome to the GPA calculator.")
 print("Please enter all your letter grades, one per line.")
 print("Enter a blank line to designate the end.")
 
-points = {'A+': 4.0, 'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.67, 'C+': 2.33, 'C': 2.0, 'C-': 1.67, 'D+': 1.33, 'D': 1.0, 'F': 0.0}
+points = {'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.70, 'C+': 2.30, 'C': 2.0, 'C-': 1.70, 'D+': 1.30, 'D': 1.0, 'D-': 0.7, 'F': 0.0}
 
 num_courses = 0
 total_points = 0
