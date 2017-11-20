ba43e8b73ab30dd910376376466dde9a95564456
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-11-20 22:16:49 +0200

    Update gpaCalc.py

--- gpaCalc.py
+++ gpaCalc.py
@@ -1,20 +1,29 @@
-print("Welcome to the GPA calculator.")
-print("Please enter all your letter grades, one per line.")
-print("Enter a blank line to designate the end.")
+import csv
 
-points = {'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.70, 'C+': 2.30, 'C': 2.0, 'C-': 1.70, 'D+': 1.30, 'D': 1.0, 'D-': 0.7, 'F': 0.0}
+print("""Welcome to the GPA calculator.
+Please enter all your letter grades, one per line.
+Enter a blank line to designate the end.""")
 
-num_courses = 0
-total_points = 0
-done = False
-while not done:
-	grade = input()
+grading = {'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.70, 'C+': 2.30, 'C': 2.0, 'C-': 1.70, 'D+': 1.30, 'D': 1.0, 'D-': 0.7, 'F': 0.0}
+
+with open('students.csv') as csvfile:
+reader = csv.DictReader(csvfile)
+for row in reader:
+	 
+
+courses = 0
+total = 0
+boolean = True
+
+while boolean:
+	grade = input().upper()
 	if grade == '':
-		done = True
-	elif grade not in points:
-		print("Unknown grade '{0}' being ignored".format(grade))
+		boolean = False
+	elif grade not in grading:
+		print("Unknown grade %s" % (grade))
 	else:
-		num_courses += 1
-		total_points += points[grade]
-if num_courses > 0:
-	print('Your GPA is {0:.3}'.format(total_points / num_courses))
+		courses += 1
+		total += grading[grade]
+
+if courses > 0:
+	print('Your GPA is %0.2f' % (total / courses))
