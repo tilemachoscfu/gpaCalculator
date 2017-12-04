d8b49cd4ca10a408807cff931258ac4cbdc5e582
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-12-04 15:04:32 +0200

    Update gpaCalc.py

--- gpaCalc.py
+++ gpaCalc.py
@@ -1,32 +1,20 @@
 import csv
 
-print("""Welcome to the GPA calculator.
-Please enter all your letter grades, one per line.
-Enter a blank line to designate the end.""")
+print("Welcome to the GPA calculator.")
 
 grading = {'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.70, 'C+': 2.30, 'C': 2.0, 'C-': 1.70, 'D+': 1.30, 'D': 1.0, 'D-': 0.7, 'F': 0.0}
 
+courses = 0
+total = 0 
+
 with open('students.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
-for row in reader:
-	x = float(row[2])
-	y = float(row[3])
-	z = float(row[4])
-	 
-
-courses = 0
-total = 0
-boolean = True
-
-while boolean:
-	grade = input().upper()
-	if grade == '':
-		boolean = False
-	elif grade not in grading:
-		print("Unknown grade %s" % (grade))
-	else:
-		courses += 1
-		total += grading[grade]
-
+	user = input('Whats your name?: ')
+	for row in reader:
+		x = row[user]
+		print(x)
+		if x in grading:
+			courses += 1
+			total += grading[x] 
 if courses > 0:
 	print('Your GPA is %0.2f' % (total / courses))
